edfb6acf66a5f79ce01773560dec18f3a3a3de32
nicapotato <nick.brooks27@gmail.com>
2025-07-03 19:25:55 +0100

    working project on mac

---
+++ start-nb.sh
@@ -0,0 +1,9 @@
+brew install sdl2
+# mac needed additional sdl2 libraries
+brew install sdl2_image sdl2_ttf
+
+git remote set-url origin https://github.com/nicapotato/Conways-Game-of-Life-C-SDL2.git
+
+cd /Users/nicapotato/Documents/repo/reading/C/Conways-Game-of-Life-C-SDL2
+
+make run
